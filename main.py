e56145bf62adee133dca3e084f97530220315f41
Keith Techasith <keithnatt.techasith@gmail.com>
2025-01-20 18:11:24 -0500

    Updated DB

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_PATH","postgresql://blog_db_keith_user:H0YarQOwonr2HxypDk3OjgvdrGOEOO1F@dpg-cu7dcvij1k6c73fk9jrg-a/blog_db_keith")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
